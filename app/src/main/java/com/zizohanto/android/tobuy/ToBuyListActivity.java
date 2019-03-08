ec64f038c7c256e9c718e470ed18227719b35b09
Zizoh Anto <zizohanto@hotmail.com>
2019-03-08 01:14:25 +0100

    first commit

---
+++ app/src/main/java/com/zizohanto/android/tobuy/ToBuyListActivity.java
@@ -0,0 +1,14 @@
+package com.zizohanto.android.tobuy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ToBuyListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_buy_list);
+    }
+}
